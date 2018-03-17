2e60486960cbc34446d88db37d9b3a0452559b1f
CEM SINAN OZTURK <cemsinan@Cems-MBP-2.hitronhub.home>
2018-03-17 01:09:50 -0700

    path added

--- pythia/tests/test_LinearRegression.py
+++ pythia/tests/test_LinearRegression.py
@@ -2,6 +2,10 @@
 # March 2018
 #
 # This script test the methods from the LinearRegression.py script.
+import sys
+import os
+sys.path.insert(0, os.path.abspath("."))
+sys.path.insert(0, os.path.abspath("../"))
 
 import pytest
 from pythia.LinearRegression import LinearRegression
@@ -18,22 +22,22 @@ def test_LinearRegression1():
     rand.seed(4)
     X = pd.DataFrame({'X1': rand.normal(size=10)})
     y = X.X1 + rand.normal(size=10)
-    
+
     # Fit a linear regression on the data
     model = LinearRegression(X, y)
-    
+
     # True value of the coefficients
     cov = np.cov(X.X1, y)
     beta = cov[0,1]/cov[0,0]
     alpha = np.mean(y) - beta*np.mean(X.X1)
     fit = alpha + beta*X.X1
     res = y - fit
-    
+
     # Test the type of the output
     assert isinstance(model.weights, dict) == True, "The model's weights don't have the right type"
     assert isinstance(model.fitted, np.ndarray) == True, "The model's fitted values don't have the right type"
     assert isinstance(model.residuals, np.ndarray) == True, "The model's residuals don't have the right type"
-    
+
     # Check the length of the output vectors
     assert len(model.weights) == 2
     assert model.fitted.shape == (10,1)
@@ -53,22 +57,22 @@ def test_LinearRegression2():
     X = pd.DataFrame({'X1': rand.normal(size=10)})
     y = X.X1 + rand.normal(size=10)
     X.X1[4] = X.X1[0]
-    
+
     # Fit a linear regression on the data
     model = LinearRegression(X, y)
-    
+
     # True value of the coefficients
     cov = np.cov(X.X1, y)
     beta = cov[0,1]/cov[0,0]
     alpha = np.mean(y) - beta*np.mean(X.X1)
     fit = alpha + beta*X.X1
     res = y - fit
-    
+
     # Test the type of the output
     assert isinstance(model.weights, dict) == True, "The model's weights don't have the right type"
     assert isinstance(model.fitted, np.ndarray) == True, "The model's fitted values don't have the right type"
     assert isinstance(model.residuals, np.ndarray) == True, "The model's residuals don't have the right type"
-    
+
     # Check the length of the output vectors
     assert len(model.weights) == 2
     assert model.fitted.shape == (10,1)
@@ -86,15 +90,15 @@ def test_LinearRegression2():
 def test_LinearRegression3():
     # Generate small data to test our function
     rand.seed(4)
-    X = pd.DataFrame({'X1': rand.normal(size=10), 
-                      'X2': rand.normal(size=10), 
+    X = pd.DataFrame({'X1': rand.normal(size=10),
+                      'X2': rand.normal(size=10),
                       'X3': rand.normal(size=10),
                       'char': ascii_letters[1:10]})
     y = X.X1 + X.X2 + X.X3 + rand.normal(size=10)
-    
+
     # Fit a linear regression on the data
     model = LinearRegression(X, y)
-    
+
     # True value of the coefficients
     X_mat = X.select_dtypes(include=[np.number], exclude=None)
     X_mat['intercept'] = pd.Series(np.ones(10), index=X_mat.index)
@@ -102,12 +106,12 @@ def test_LinearRegression3():
     beta = np.linalg.inv(np.transpose(X_mat)@X_mat)@np.transpose(X_mat)@np.array(y)
     fit = X_mat@beta
     res = y - fit
-    
+
     # Test the type of the output
     assert isinstance(model.weights, dict) == True, "The model's weights don't have the right type"
     assert isinstance(model.fitted, np.ndarray) == True, "The model's fitted values don't have the right type"
     assert isinstance(model.residuals, np.ndarray) == True, "The model's residuals don't have the right type"
-    
+
     # Check the length of the output vectors
     assert len(model.weights) == 4
     assert model.fitted.shape == (10,1)
@@ -122,15 +126,15 @@ def test_LinearRegression3():
 def test_LinearRegression4():
     # Generate small data to test our function
     rand.seed(4)
-    X = pd.DataFrame({'X1': rand.normal(size=10), 
-                      'X2': rand.normal(size=10), 
+    X = pd.DataFrame({'X1': rand.normal(size=10),
+                      'X2': rand.normal(size=10),
                       'X3': rand.normal(size=10)})
     y = X.X1 + X.X2 + X.X3 + rand.normal(size=10)
-    
+
     # Add some missing values
     X.X1[3] = None
     X.X3[5] = None
-    
+
     try:
         LinearRegression(X, y)
     except NameError:
@@ -144,11 +148,10 @@ def test_LinearRegression5():
     # Generate small data to test our function
     X = pd.DataFrame({'char': [ascii_letters[i] for i in range(10)]})
     y = range(10)
-    
+
     try:
         LinearRegression(X, y)
     except NameError:
         assert True
     else:
         assert False
-    
